1fb214f6ea2f49bac558f900bbc6440674717e4f
Rothleer <rothleer@outlook.com>
2022-08-20 22:10:33 +0800

    feat: 第一次提交

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,15 @@ import { defaultValueCtx, Editor } from "@milkdown/core";
 import { nord } from "@milkdown/theme-nord";
 import { commonmark } from "@milkdown/preset-commonmark";
 import { emoji } from "@milkdown/plugin-emoji";
+import { math } from '@milkdown/plugin-math';
+import { RemarkPlugin } from '@milkdown/core';
+import directive from 'remark-directive';
+
+import 'katex/dist/katex.min.css';
+
+const iframe = createNode(() => {
+
+});
 
 Editor.make()
   .config((ctx) => {
@@ -9,5 +18,10 @@ Editor.make()
   })
   .use(nord)
   .use(commonmark)
+  .use(math)
   .use(emoji)
   .create();
+function createNode(arg0: () => void) {
+  throw new Error("Function not implemented.");
+}
+
